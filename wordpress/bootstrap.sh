ba43ba0a3aa301117d23a7f473e3db1add8e0226
Elric Best <spiritofelric@gmail.com>
2016-07-05 19:02:18 -0400

    fixed wordpress 3 on ubuntu 14.04 installation

--- wordpress/bootstrap.sh
+++ wordpress/bootstrap.sh
@@ -20,32 +20,29 @@ sudo apt-get install -y nodejs
 if [ ! -h /var/www ];
 then
 #apache
-    #rm -rf /var/www
-    #sudo ln -s /home/app/public /var/www
     sed -i '/AllowOverride None/c AllowOverride All' /etc/apache2/sites-available/000-default.conf
     a2enmod rewrite && a2enmod vhost_alias
     mv /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.old
     cp /home/app/000-default.conf /etc/apache2/sites-available/000-default.conf
+#    cp /home/app/wordpress.conf /etc/apache2/sites-available/wordpress.conf
+#    sudo a2ensite wordpress
+#    systemctl restart apache2.service
 
 #php
-    #sed -i '/expose_php = On/c expose_php = Off' /etc/php5/apache2/php.ini
-    #sed -i '/allow_url_fopen = On/c allow_url_fopen = Off' /etc/php5/apache2/php.ini
-    chown -R www-data:www-data /var/www/html/
+    chown -R www-data:www-data /var/www
 
 #wordpress
     chown -R www-data:www-data /usr/share/wordpress
-    ln -s /usr/share/wordpress /var/www/html/wordpress
-    sudo chown -h www-data:www-data /var/www/html/wordpress
     gzip -d /usr/share/doc/wordpress/examples/setup-mysql.gz
     bash /usr/share/doc/wordpress/examples/setup-mysql -n wordpress localhost
-    cp /etc/wordpress/config-localhost.php /etc/wordpress/config-127.0.0.1.php
-    chown www-data:www-data /etc/wordpress/config-127.0.0.1.php
+    ln -s /etc/wordpress/config-localhost.php /etc/wordpress/config-127.0.0.1.php
+    sed -i '/define("WP_CONTENT_DIR", "/srv/www/wp-content/localhost");/c //' /etc/wordpress/config-localhost.php
+    sed -i '/?>/c define("FS_METHOD", "direct");\ndefine("WP_CONTENT_DIR", "/usr/share/wordpress/wp-content");\ndefine("WP_PLUGIN_DIR", "/usr/share/wordpress/wp-content/plugins");\n?>' /etc/wordpress/config-localhost.php
+    chown -h www-data:www-data /etc/wordpress/config-127.0.0.1.php
+    chown www-data:www-data /etc/wordpress/config-localhost.php
     find /usr/share/wordpress/wp-content -type d -exec chmod -R 775 {} \;
     find /usr/share/wordpress/wp-content -type f -exec chmod -R 664 {} \;
     #/var/log/apache2/error.log - check for errors
-
-    sed -i '/?>/c define("FS_METHOD", "direct");\n?>' /etc/wordpress/config-127.0.0.1.php
-    #cat "define('FS_METHOD', 'direct');define('FTP_BASE', '/usr/share/wordpress/');define('FTP_CONTENT_DIR', '/usr/share/wordpress/wp-content/');define('FTP_PLUGIN_DIR','/usr/share/wordpress/wp-content/plugins/');" >> /etc/wordpress/config-127.0.0.1.php
 
     sudo /etc/init.d/apache2 restart
 fi
